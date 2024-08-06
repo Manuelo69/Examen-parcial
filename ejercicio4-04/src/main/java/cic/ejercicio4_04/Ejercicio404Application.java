7e2f8a636bbed67b50e272f3681644bc5a33dfe6
Manuelo69 <nomail@gmail.com>
2024-08-06 10:41:51 +0200

    Carpeta ejercicio 4

---
+++ ejercicio4-04/src/main/java/cic/ejercicio4_04/Ejercicio404Application.java
@@ -0,0 +1,13 @@
+package cic.ejercicio4_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio404Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio404Application.class, args);
+	}
+
+}
