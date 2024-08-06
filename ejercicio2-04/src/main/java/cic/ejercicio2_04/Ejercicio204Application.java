a14cd3aa73bfa0bdd432dc1fee4810b06f6da76a
Manuelo69 <nomail@gmail.com>
2024-08-06 09:57:46 +0200

    ejercicio 2, me gusta sufrir :), pero no tanto como para ir a por el 5

---
+++ ejercicio2-04/src/main/java/cic/ejercicio2_04/Ejercicio204Application.java
@@ -0,0 +1,13 @@
+package cic.ejercicio2_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio204Application.class, args);
+	}
+
+}
---
+++ ejercicio2-04/src/test/java/cic/ejercicio2_04/Ejercicio204ApplicationTests.java
@@ -0,0 +1,13 @@
+package cic.ejercicio2_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio204ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
